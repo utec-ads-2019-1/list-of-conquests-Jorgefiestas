e01db09aea096e7006050ff559f6107d5680c70f
Jorge Ignacio Fiestas Lavezzolo <jorge@amigocloud.com>
2019-03-28 21:05:55 -0500

    Adds Solution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
+int n;
+map<string, int> cnt;
 
 int main(int argc, char *argv[]) {
+	cin>>n;
+	cin.ignore();
+	for(int i = 0; i<n; i++){
+		string line;
+		getline(cin, line);
+		stringstream ss(line);
+		string country;
+		getline(ss, country, ' ');
+		cnt[country]++;
+	}
+	for(auto it : cnt)
+		cout<<it.first<<" "<<it.second<<endl;
     return 0;
 }

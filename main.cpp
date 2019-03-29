8b913a8c2632e83e1cfda45c9556755c723bc31a
Jorge Ignacio Fiestas Lavezzolo <jorge@amigocloud.com>
2019-03-29 15:33:50 -0500

    Improves coding standrds

--- main.cpp
+++ main.cpp
@@ -3,18 +3,24 @@ using namespace std;
 int n;
 map<string, int> cnt;
 
+void process_line(string &);
+
 int main(int argc, char *argv[]) {
 	cin>>n;
 	cin.ignore();
+	string line;
 	for(int i = 0; i<n; i++){
-		string line;
 		getline(cin, line);
-		stringstream ss(line);
-		string country;
-		getline(ss, country, ' ');
-		cnt[country]++;
+		process_line(line);
 	}
 	for(auto it : cnt)
 		cout<<it.first<<" "<<it.second<<endl;
     return 0;
+}
+
+void process_line(string &str){
+	stringstream ss(str);
+	string country;
+	ss>>country;
+	cnt[country]++;
 }
